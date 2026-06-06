0d5cd2298f03cc4f375bc462b80b1f5eb7470a68
Kevin <171992754+KevCast1604@users.noreply.github.com>
2026-06-05 23:20:30 -0500

    refact: exclude ReactiveSecurityAutoConfiguration in gateway main class

--- src/main/java/com/campusdrive/uniride/UnirideApplication.java
+++ src/main/java/com/campusdrive/uniride/UnirideApplication.java
@@ -3,7 +3,7 @@ package com.campusdrive.uniride;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration.class })
 public class UnirideApplication {
 
 	public static void main(String[] args) {
